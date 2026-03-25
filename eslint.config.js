1a7a2ce5a1c66af2101997cb75b47d27aee63c02
Brandon Corbett <Bccorb@users.noreply.github.com>
2026-03-24 23:27:15 -0400

    ci: fix formatting issues

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,9 @@ import prettier from 'eslint-config-prettier';
 
 export default [
   {
+    ignores: ['dist', 'node_modules', 'coverage'],
+  },
+  {
     files: ['**/*.ts'],
     languageOptions: {
       parser: tsParser,
